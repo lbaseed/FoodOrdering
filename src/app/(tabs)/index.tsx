f020b977e39319eeaa50a0a7bb7dad666a9055a9
Abdulbasid Haruna <lbaseed2010@gmail.com>
2024-02-17 23:50:11 +0100

    Initial Commit

---
+++ src/app/(tabs)/index.tsx
@@ -0,0 +1,39 @@
+import { Image, StyleSheet } from "react-native";
+
+import EditScreenInfo from "@/src/components/EditScreenInfo";
+import { Text, View } from "@/src/components/Themed";
+import Colors from "@/src/constants/Colors";
+import products from "@/assets/data/products";
+
+const product = products[0];
+
+export default function TabOneScreen() {
+	return (
+		<View style={styles.container}>
+      <Image source={{  uri: product.image }} style={styles.productImage} />
+			<Text style={styles.title}>{product?.name}</Text>
+			<Text style={styles.price}>$ {product?.price}</Text>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 10
+  },
+	title: {
+		fontSize: 20,
+		fontWeight: "600",
+		marginVertical: 10
+	},
+	price: {
+		color: Colors.light.tint,
+		fontWeight: "bold"
+	},
+  productImage: {
+    width: '100%',
+    aspectRatio: 1
+  }
+});
